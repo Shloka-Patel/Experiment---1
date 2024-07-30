3e7a5bdde222eb1a5043542e2917fe81914e673c
Shloka-Patel <137433503+Shloka-Patel@users.noreply.github.com>
2024-07-30 21:17:24 +0530

    Add files via upload

---
+++ Shloka_B2_120_Exp1A_CDS.cpp
@@ -0,0 +1,11 @@
+// NAME - SHLOKA PATEL 
+// PRN - 23070123120 
+// EXPERIMENT - 1(A) 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"HELLO WORLD!";     //Output - HELLO WORLD!
+    return 0;
+}
